bdc2cc264351ebded3a0bee6457d459c4d6d7077
chienlx1005 <chienlx1005@gmail.com>
2022-06-20 17:20:03 +0700

    add congif

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 // const express = require("express");
-
+// require("dotenv").config();
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;
+console.log(">>check port: ", port);
 configViewEngine(app);
 
 app.get("/", (req, res) => {
